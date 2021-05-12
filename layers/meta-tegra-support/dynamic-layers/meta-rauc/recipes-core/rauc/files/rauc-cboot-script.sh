23e94e6994f46b3df42f8f73df66b80db7b8bf0d
Matt Madison <matt@madison.systems>
2021-05-12 14:40:18 -0700

    meta-tegra-support: update RAUC bootloader script, config file
    - add keyring to config file
    - update cboot script to use 'slot<n>' for the slot names
    
    Signed-off-by: Matt Madison <matt@madison.systems>

--- layers/meta-tegra-support/dynamic-layers/meta-rauc/recipes-core/rauc/files/rauc-cboot-script.sh
+++ layers/meta-tegra-support/dynamic-layers/meta-rauc/recipes-core/rauc/files/rauc-cboot-script.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 get_state() {
-    local statusline=$(tegra-boot-control --status | grep "^Slot $1:")
+    local slot=${1##slot}
+    local statusline=$(tegra-boot-control --status | grep "^Slot $slot:")
     [ -n "$statusline" ] || return 1
     if echo "$statusline" | grep -q "successful: YES"; then
 	echo "good"
@@ -14,19 +15,31 @@ get_state() {
 set_state() {
     local curslot=$(tegra-boot-control --current-slot)
     [ -n "$curslot" -a -n "$1" ] || return 1
-    if [ $curslot -ne $1 -o "$2" != "good" ]; then
+    if [ "slot$curslot" != "$1" -o "$2" != "good" ]; then
 	echo "Unsupported operation: set-state $1 $2" >&2
 	return 1
     fi
     tegra-boot-control --mark-successful
 }
 
+get_primary() {
+    local curslot=$(tegra-boot-control --current-slot)
+    [ -n "$curslot" ] || return 1
+    echo "slot$curslot"
+}
+
+set_primary() {
+    local slot=${1##slot}
+    [ "$slot" = "0" -o "$slot" = "1" ] || return 1
+    tegra-boot-control --set-active $slot
+}
+
 case "$1" in
     get-primary)
-	tegra-boot-control --current-slot
+	get_primary "$2"
 	;;
     set-primary)
-	tegra-boot-control --set-active "$2"
+	set_primary "$2"
 	;;
     get-state)
 	get_state "$2"
