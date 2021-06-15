4022cc13c92ca937a175484a0becee9844f0ec56
Matt Madison <matt@madison.systems>
2021-06-15 10:53:36 -0700

    mender-client: add bbappend for 2.6.0, update machine ID script
    - use local fork for mender-client sources, with patches to
      handle systemd rootfs volatile overlay
    - update machine ID script to read/set tegra-bootinfo variable
      (mainly for cboot platforms)
    
    Signed-off-by: Matt Madison <matt@madison.systems>

--- layers/meta-testdistro/external/meta-mender-core/recipes-mender/mender-client/mender-client/mender-client-set-systemd-machine-id.sh
+++ layers/meta-testdistro/external/meta-mender-core/recipes-mender/mender-client/mender-client/mender-client-set-systemd-machine-id.sh
@@ -4,16 +4,19 @@
 # so that it will persist between updates.
 #
 
-FUSED_ID=$(tegra-fuse-tool --show-machine-id)
+FUSED_ID=$(tegra-fuse-tool --show-machine-id 2>/dev/null)
 [ -z "$FUSED_ID" ] || exit 0
 
 CURRENT_BOOTLOADER_ID=$(fw_printenv -n mender_systemd_machine_id 2>/dev/null)
+[ -n "$CURRENT_BOOTLOADER_ID" ] || CURRENT_BOOTLOADER_ID=$(tegra-bootinfo -v -n machine_id 2>/dev/null)
 CURRENT_SYSTEMD_ID=$(cat /etc/machine-id)
 
 rc=0
 if [ -z "${CURRENT_BOOTLOADER_ID}" ] && [ ! -z "${CURRENT_SYSTEMD_ID}" ]; then
-    fw_setenv "mender_systemd_machine_id" "${CURRENT_SYSTEMD_ID}"
-    rc=$?
+    fw_setenv "mender_systemd_machine_id" "${CURRENT_SYSTEMD_ID}" || rc=1
+    if type tegra-bootinfo 2>&1 >/dev/null; then
+	tegra-bootinfo -V machine_id "${CURRENT_SYSTEMD_ID}" || rc=1
+    fi
 elif [ "${CURRENT_BOOTLOADER_ID}" != "${CURRENT_SYSTEMD_ID}" ]; then
     echo "Error; bootloader and systemd disagree on machine-id." >&2
     rc=1
